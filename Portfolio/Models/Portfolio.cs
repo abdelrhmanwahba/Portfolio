850fc74d51b8d87ff652e01e29fe24131f0f02f0
Abdelrahman Khaled <Abdelrahman.Khaled16@fci.helwan.edu.eg>
2020-11-06 21:59:24 +0200

    project model and view model added (microsoft.mvc.future) must be installed

---
+++ Portfolio/Models/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Portfolio
+    {
+
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Portfolio/ViewModels/ProjectFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Portfolio.ViewModels
+{
+    public class ProjectFormViewModel
+    {
+        public int? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+        [Microsoft.Web.Mvc.FileExtensions(Extensions = ("png,jpg"),
+            ErrorMessage = "Specify a png or jpg photo")]
+        [Display(Name = "Project image")]
+        [Required]
+
+        public HttpPostedFileBase File { get; set; }
+    }
+}
