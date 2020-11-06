d56c0af7adec1adaa1ed2843917e9eb8928ff0d9
Abdelrahman Khaled <Abdelrahman.Khaled16@fci.helwan.edu.eg>
2020-11-06 23:16:55 +0200

    "AddProject" Action

--- Portfolio/Controllers/ProjectsController.cs
+++ Portfolio/Controllers/ProjectsController.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
+using Portfolio.Models;
+using Portfolio.ViewModels;
 
 namespace Portfolio.Controllers
 {
     public class ProjectsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context =new ApplicationDbContext();
+        }
         // GET: Projects
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddProject()
+        {
+
+            return View();
+        }
+
+        public ActionResult Save(ProjectFormViewModel projectFormViewModel)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("AddProject", projectFormViewModel);
+            }
+
+            var path = "~/images/" + Path.GetFileName(projectFormViewModel.File.FileName);
+
+
+            var project = new Project
+
+            {
+                Name = projectFormViewModel.Name,
+                Description = projectFormViewModel.Description,
+                Image = path
+
+            };
+            _context.Projects.Add(project);
+            projectFormViewModel.File.SaveAs(Server.MapPath(path));
+            _context.SaveChanges();
+
+            return View("index");
         }
     }
 }
