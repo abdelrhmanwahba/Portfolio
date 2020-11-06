fa6478d4151a7eb51308ec5b5184bf4f772a1a32
Abdelrahman Khaled <Abdelrahman.Khaled16@fci.helwan.edu.eg>
2020-11-06 23:33:31 +0200

    Validation messages styling

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Portfolio
 {
@@ -24,7 +23,7 @@ namespace Portfolio
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
